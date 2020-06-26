6732f4b556aeec9963d9811c685438ec8e38dce3
Mike Beaudry <michaelbeaudry93@gmail.com>
2020-06-26 01:04:46 -0400

    updating our app with a shop page, collection preview component, and collection item, added webpack.config.js

---
+++ build/precache-manifest.6856f1779f28c4cfd2be053142d23306.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f36013b974042b08a1ac5e883609b467",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5ca50401df1d36a694e",
+    "url": "/static/css/main.1a454d87.chunk.css"
+  },
+  {
+    "revision": "5e0f0f582bd4ed78975b",
+    "url": "/static/js/2.3848b52f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3848b52f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5ca50401df1d36a694e",
+    "url": "/static/js/main.28128ffb.chunk.js"
+  },
+  {
+    "revision": "964c8eb06f516d2f5330",
+    "url": "/static/js/runtime-main.0fd63531.js"
+  }
+]);
---
+++ src/components/preview-collection/PreviewCollection.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import CollectionItem from '../collection-item/CollectionItem'
+
+import './preview-collection.styles.scss'
+
+function PreviewCollection({ title, items }) {
+    return (
+        <div className="collection-preview">
+            <h1 className="title">{title.toUpperCase()}</h1>
+            <div className="preview">
+                {
+                    items
+                    .filter((item, index) => index < 4)
+                    .map(({id, ...itemProps}) => (
+                        <CollectionItem key={id} {...itemProps} />
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
+
+export default PreviewCollection
