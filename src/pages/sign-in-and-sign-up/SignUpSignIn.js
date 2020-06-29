33e454dd76736de1d9cf61c3b3c449a76cacca41
Mike Beaudry <michaelbeaudry93@gmail.com>
2020-06-29 01:42:58 -0400

    added sign-up component, authenticates and creates new user profile document

--- src/pages/sign-in-and-sign-up/SignUpSignIn.js
+++ src/pages/sign-in-and-sign-up/SignUpSignIn.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import SignIn from '../../components/sign-in/SignIn'
+import SignUp from '../../components/sign-up/SignUp'
 
 import './sign-in-and-sign-up.styles.scss'
 
@@ -8,6 +9,7 @@ function SignUpSignIn() {
     return (
         <div className='sign-in-and-sign-up'>
             <SignIn />
+            <SignUp />
         </div>
     )
 }
